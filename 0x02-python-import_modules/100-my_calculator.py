ef2b9251a3cebc9a090cb7e82605a7a5697cec9a
Gashaw D <42271526+gashawdemlew@users.noreply.github.com>
2022-07-04 21:01:57 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc != 3:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
+from sys import argv
 
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-    else:
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    f = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in f:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
-    
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
